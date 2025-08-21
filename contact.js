aad2888ab5e1990306c83d3c3db7cc35adfa1e3e
liweimin38 <3984781015@qq.com>
2025-08-21 23:32:49 +0800

    admin upload /contact.js

---
+++ contact.js
@@ -0,0 +1,57 @@
+// contact.js - 联系页面组件
+export default {
+    template: `
+        <div class="page-content">
+            <h1><i class="fas fa-address-card"></i>联系方式</h1>
+            
+            <div class="contact-section">
+                <h2><i class="fab fa-qq"></i> QQ群</h2>
+                <div class="contact-info">
+                    <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=MYPpUa94dG1KRtd8J9Pf0Voc5zZS0UhN&authKey=E12NGuCr8UmL3BtBmcX3%2FewNUDfxwqlqJP9maZ6Ias7DqJQaXbEoOSpbom3WTrbo&noverify=0&group_code=757291507" target="_blank">
+                        <img src="src/qq.jpg" alt="QQ群二维码">
+                    </a>
+                    <div>
+                        <p>点击上方二维码或<a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=MYPpUa94dG1KRtd8J9Pf0Voc5zZS0UhN&authKey=E12NGuCr8UmL3BtBmcX3%2FewNUDfxwqlqJP9maZ6Ias7DqJQaXbEoOSpbom3WTrbo&noverify=0&group_code=757291507" target="_blank">此处链接</a>加入QQ群</p>
+                        <p>群号: 757291507</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="contact-section">
+                <h2><i class="fab fa-qq"></i> 个人QQ</h2>
+                <div class="contact-info">
+                    <img src="src/Individualqq.jpg" alt="个人QQ二维码">
+                    <div>
+                        <p>扫描二维码添加我的个人QQ</p>
+                        <p>个人QQ号：3984781015</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="contact-section">
+                <h2><i class="fab fa-tiktok"></i> 抖音</h2>
+                <div class="contact-info">
+                    <a href="https://v.douyin.com/i5sGK56r/" target="_blank">
+                        <img src="src/douyin.jpg" alt="抖音二维码">
+                    </a>
+                    <div>
+                        <p>点击上方二维码或<a href="https://v.douyin.com/i5sGK56r/" target="_blank">此处链接</a>访问我的抖音</p>
+                        <p>个人抖音号：49209322265</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="contact-section">
+                <h2><i class="fab fa-weixin"></i> 微信</h2>
+                <div class="contact-info">
+                    <a href="https://liweimin3843.dpdns.org/file/wx.png" target="_blank">
+                        <img src="src/wx.png" alt="微信二维码">
+                    </a>
+                    <div>
+                        <p>扫描二维码添加我的微信</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+};
